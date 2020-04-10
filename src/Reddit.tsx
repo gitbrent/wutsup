025fdf48cd4fc4382e27bd720e4fcd8a9b4f57b9
Brent Ely <gitbrent@gmail.com>
2020-04-09 23:09:15 -0500

    big UI improvements

--- src/Reddit.tsx
+++ src/Reddit.tsx
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
+enum redditSub {
+	askreddit = 'askreddit',
+	memes = 'memes',
+	politics = 'politics',
+}
+enum sortType {
+	controversial = 'controversial',
+	hot = 'hot',
+	new = 'new',
+	rising = 'rising',
+	top = 'top',
+}
 interface post {
 	subreddit: string // "politics"
 	subreddit_subscribers: number
@@ -28,20 +40,23 @@ interface post {
 		]
 	}
 }
-
 interface subJson {
 	kind: string
 	data: post
 }
 
 export default function Reddit() {
-	const [delaySecs, setDelaySecs] = useState(10)
-	const [selReddit, setSelReddit] = useState('politics')
+	const [delaySecs, setDelaySecs] = useState(30)
+	const [selRedditSub, setSelRedditSub] = useState<redditSub | string>(redditSub.politics)
+	const [selSortType, setSelSortType] = useState<sortType | string>(sortType.top)
 	const [posts, setPosts] = useState<post[]>([])
 	const [idxToShow, setIdxToShow] = useState(0)
 	const [isActive, setIsActive] = useState(true)
 	const [seconds, setSeconds] = useState(0)
 
+	/**
+	 * @desc Display loop
+	 */
 	useEffect(() => {
 		setIdxToShow(idxToShow + 1)
 
@@ -67,11 +82,11 @@ export default function Reddit() {
 
 	useEffect(() => {
 		fetchSelSub()
-	}, [selReddit])
+	}, [selRedditSub])
 
 	function fetchSelSub() {
-		fetch(`https://www.reddit.com/r/${selReddit}/rising.json`)
-		//fetch(`https://www.reddit.com/r/${selReddit}.json`)
+		// FYI: sortType is optional
+		fetch(`https://www.reddit.com/r/${selRedditSub}/${selSortType}.json`)
 			.then(response => response.json())
 			.then(json => {
 				let posts: post[] = []
@@ -80,7 +95,7 @@ export default function Reddit() {
 						subreddit: child.data.subreddit,
 						subreddit_subscribers: child.data.subreddit_subscribers,
 						selftext: child.data.selftext,
-						title: child.data.title,
+						title: (child.data.title||'').replace(/\&amp\;/gi,'&'),
 						permalink: child.data.permalink,
 						link_flair_text: child.data.link_flair_text,
 						thumbnail: child.data.thumbnail,
@@ -94,7 +109,7 @@ export default function Reddit() {
 						//preview: child.data.preview,
 						created: child.data.created,
 						created_utc: child.data.created_utc,
-						dateCreated: new Date(child.data.created*1000),
+						dateCreated: new Date(child.data.created * 1000),
 						author: child.data.author,
 					})
 				})
@@ -110,7 +125,7 @@ export default function Reddit() {
 	function getApiData() {
 		//fetch(`https://www.reddit.com/api/trending_subreddits.json`) // WORKS
 		fetch(`https://www.reddit.com/api/trending_subreddits.json`)
-		/*
+			/*
 			fetch('https://www.reddit.com/r/askreddit.json', {
 				method: 'GET',
 				headers: {
@@ -140,63 +155,111 @@ export default function Reddit() {
 	}
 
 	return (
-		<div>
-			<h3>REDDIT</h3>
-			<h5>AskReddit</h5>
-			<div>
-				<div className='row'>
-					<div className='col'>
-						<select className='form-control' value={selReddit} onChange={event => setSelReddit(event.currentTarget.value)}>
-							<option value='askreddit'>r/askreddit</option>
-							<option value='memes'>r/memes</option>
-							<option value='politics'>r/politics</option>
-						</select>
-					</div>
-					<div className='col-auto'>
-						<select className='form-control' value={delaySecs} onChange={event => setDelaySecs(Number(event.currentTarget.value))}>
-							<option value='5'>5</option>
-							<option value='10'>10</option>
-							<option value='30'>30</option>
-							<option value='60'>60</option>
-						</select>
-					</div>
-				</div>
+		<div className='card mb-0'>
+			<div className='card-header bg-primary text-white text-center'>
+				<h3 className='mb-0'>REDDIT</h3>
 			</div>
-			<section>
-				{(posts || [])
-					.filter((_post, idx) => idx >= idxToShow)
-					.map((post, idx) => (
-						<div className='card mb-0' key={idx}>
-							<div className='card-header bg-secondary'>
-								<div className='row'>
-									{post.thumbnail && post.thumbnail !== 'default' && post.thumbnail !== 'self' && (
-										<div className='col-auto'>
-											<img src={post.thumbnail} style={{ maxWidth: '100px', maxHeight: '60px' }} />
-										</div>
-									)}
-									<div className='col'>
-										<h6 className='text-white mb-0'>{post.title}</h6>
-										<div>{post.dateCreated.toDateString()}</div>
-									</div>
-									<div className='col-auto'>
-										<h6 className='text-white mb-0'>{post.num_comments}</h6>
-									</div>
-									<div className='col-auto'>
-										<h6 className='text-white mb-0'>{post.ups}</h6>
-									</div>
+			<div className='card-body'>
+				<section className='mb-3' data-desc='filters'>
+					<div className='row'>
+						<div className='col'>
+							<select className='form-control' value={selRedditSub} onChange={event => setSelRedditSub(event.currentTarget.value)}>
+								{Object.entries(redditSub).map(([key, val], idx) => (
+									<option key={`rsub${idx}`} value={`${key}`}>{`${val}`}</option>
+								))}
+							</select>
+						</div>
+						<div className='col-auto'>
+							<select className='form-control' value={selSortType} onChange={event => setSelSortType(event.currentTarget.value)}>
+								{Object.entries(sortType).map(([key, val], idx) => (
+									<option key={`sort${idx}`} value={`${key}`}>{`${val}`}</option>
+								))}
+							</select>
+						</div>
+						<div className='col-auto'>
+							<select className='form-control' value={delaySecs} onChange={event => setDelaySecs(Number(event.currentTarget.value))}>
+								<option value='5'>5</option>
+								<option value='10'>10</option>
+								<option value='30'>30</option>
+								<option value='60'>60</option>
+							</select>
+						</div>
+					</div>
+				</section>
+
+				<section className='mb-0' data-desc='picture posts'>
+					{posts
+						.filter((_post, idx) => idx >= idxToShow)
+						.filter(post => post.thumbnail && post.url && post.url.toLowerCase().endsWith('jpg'))
+						.map((post, idx) => (
+							<div key={idx} className='row'>
+								<div className='col'>
+									<img src={post.url} alt='thumbnail' style={{ maxWidth: '600px', maxHeight: '300px' }} />
 								</div>
 							</div>
-							{post.thumbnail && post.url && post.url.toLowerCase().endsWith('jpg') && (
-								<div className='card-body'>
-									<img src={post.url} style={{ maxWidth: '600px', maxHeight: '300px' }} />
+						))}
+				</section>
+
+				<section className='mb-0' data-desc='regular posts'>
+					{posts
+						.filter((_post, idx) => idx >= idxToShow)
+						.filter(post => !post.url || !(post.url || '').toLowerCase().endsWith('jpg'))
+						.map((post, idx) => (
+							<div key={idx} className='row mb-4'>
+								{post.thumbnail && post.thumbnail !== 'default' && post.thumbnail !== 'self' && (
+									<div className='col-auto text-center' style={{ minWidth: '130px' }}>
+										<img src={post.thumbnail} alt='thumbnail' style={{ maxWidth: '100px', maxHeight: '60px' }} />
+									</div>
+								)}
+								<div className='col'>
+									<h5 className='text-white mb-0'>{post.title}</h5>
+									<code className='text-white-50'>
+										<svg
+											className='bi bi-calendar mr-1'
+											width='1em'
+											height='1em'
+											viewBox='0 0 16 16'
+											fill='currentColor'
+											xmlns='http://www.w3.org/2000/svg'>
+											<path
+												fill-rule='evenodd'
+												d='M14 0H2a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z'
+												clip-rule='evenodd'
+											/>
+											<path
+												fill-rule='evenodd'
+												d='M6.5 7a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm-9 3a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm-9 3a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2z'
+												clip-rule='evenodd'
+											/>
+										</svg>
+										{post.dateCreated.toDateString()}
+									</code>
 								</div>
-							)}
-							{/*
-							<div className='card-footer'>{post.author}</div>
-						*/}
-						</div>
-					))}
-			</section>
+								<div className='col-auto text-center text-primary'>
+									<h5>{post.num_comments}</h5>
+									<svg className='bi bi-chat h5' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+										<path
+											fill-rule='evenodd'
+											d='M2.678 11.894a1 1 0 01.287.801 10.97 10.97 0 01-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 01.71-.074A8.06 8.06 0 008 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 01-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 00.244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.52.263-1.639.742-3.468 1.105z'
+											clip-rule='evenodd'
+										/>
+									</svg>
+								</div>
+								<div className='col-auto text-center text-warning'>
+									<h5>{post.ups}</h5>
+									<svg className='bi bi-arrow-up h5' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+										<path fill-rule='evenodd' d='M8 3.5a.5.5 0 01.5.5v9a.5.5 0 01-1 0V4a.5.5 0 01.5-.5z' clip-rule='evenodd' />
+										<path
+											fill-rule='evenodd'
+											d='M7.646 2.646a.5.5 0 01.708 0l3 3a.5.5 0 01-.708.708L8 3.707 5.354 6.354a.5.5 0 11-.708-.708l3-3z'
+											clip-rule='evenodd'
+										/>
+									</svg>
+								</div>
+							</div>
+						))}
+				</section>
+			</div>
 		</div>
 	)
 }
