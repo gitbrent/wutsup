ed87e1a62da775b0457f5daf518d645170b75fd4
Brent Ely <gitbrent@gmail.com>
2020-04-16 23:42:49 -0500

    initial file; content placeholder

---
+++ src/Twitter.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+/*
+const Twitter = require('twitter');
+const client = new Twitter({
+  consumer_key: 'xxxxxxx',
+  consumer_secret: 'xxxxxxxx',
+  access_token_key: 'xxxxxxx',
+  access_token_secret: 'xxxxxxxx',
+})
+
+class TwitterFeed extends Component {
+  componentDidMount() {
+    const params = {screen_name: 'nimaiwalsh'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+          console.log(tweets);
+      } else {
+          throw error
+      }
+    });
+  }
+
+  render() {
+    return(
+      <div>Test twitter feed</div>
+    )
+  }
+}
+
+export default Twitter
+// https://api.twitter.com/1.1/statuses/user_timeline.json
+*/
