79465a019303586ba1e66623054ccbeec9b308d1
Brent Ely <gitbrent@gmail.com>
2020-04-18 23:55:05 -0500

    layout work

--- src/AppBodyNew.tsx
+++ src/AppBodyNew.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Box, Grow, Grid } from '@material-ui/core'
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+import Reddit from './RedditNew'
+
 import { makeStyles } from '@material-ui/core/styles'
 const useStylesGrp = makeStyles(theme => ({
 	root: {
@@ -20,6 +22,9 @@ const useStylesBtn = makeStyles(_theme => ({
 		flex: '1',
 		//margin: theme.spacing(0), // just testing 'theme'
 		padding: '5px 15px',
+		"&:hover":{
+			border: '1px solid rgba(144, 202, 249, 0.75) !important',
+		}
 	},
 	selected: {
 		backgroundColor: 'rgba(144, 202, 249, 0.25) !important',
@@ -41,10 +46,36 @@ export default function Testbed() {
 
 	const [viewSelected, setViewSelected] = useState<MainView | string>(MainView.reddit)
 
+	function renderBoxesWithGrow(): JSX.Element {
+		return (
+			<Grid container>
+				<Grid item xs={12}>
+					<Box m={4}>
+						<Grow in={true}>
+							<Box bgcolor='background.paper' p={5}>
+								I JUST FADE IN DEFAULT
+								<ArrowUpwardIcon />
+							</Box>
+						</Grow>
+
+						<Box mb={4} />
+
+						<Grow in={true} style={{ transformOrigin: '0 0 0' }} {...{ timeout: 1000 }}>
+							<Box bgcolor='background.paper' p={5}>
+								I TAKE 1 SECOND TO FADE IN
+								<ArrowUpwardIcon />
+							</Box>
+						</Grow>
+					</Box>
+				</Grid>
+			</Grid>
+		)
+	}
+
 	return (
 		<>
 			<header>
-				<Box mb={4}>
+				<Box p={0} mb={1}>
 					<ToggleButtonGroup
 						classes={classesGrp}
 						size='small'
@@ -60,27 +91,12 @@ export default function Testbed() {
 				</Box>
 			</header>
 			<main>
-				<Grid container>
-					<Grid item xs={12}>
-						<Box m={4}>
-							<Grow in={viewSelected === MainView.reddit}>
-								<Box bgcolor='background.paper' p={5}>
-									I JUST FADE IN DEFAULT
-									<ArrowUpwardIcon />
-								</Box>
-							</Grow>
-
-							<Box mb={4} />
-
-							<Grow in={viewSelected === MainView.reddit} style={{ transformOrigin: '0 0 0' }} {...(viewSelected === MainView.reddit ? { timeout: 1000 } : {})}>
-								<Box bgcolor='background.paper' p={5}>
-									I TAKE 1 SECOND TO FADE IN
-									<ArrowUpwardIcon />
-								</Box>
-							</Grow>
-						</Box>
-					</Grid>
-				</Grid>
+				<Box p={0}>
+					{viewSelected === MainView.reddit && <Reddit />}
+					{viewSelected === MainView.newsWorld && renderBoxesWithGrow()}
+					{viewSelected === MainView.twitter && <div>(need a developer key)</div>}
+					{viewSelected === MainView.weather && <div>(need a weather source)</div>}
+				</Box>
 			</main>
 		</>
 	)
