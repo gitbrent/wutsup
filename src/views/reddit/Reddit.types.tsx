75203670fb134a403f4f98b658e2648b18145c23
Brent Ely <gitbrent@gmail.com>
2020-05-07 23:49:59 -0500

    added Comment

--- src/views/reddit/Reddit.types.tsx
+++ src/views/reddit/Reddit.types.tsx
@@ -58,3 +58,73 @@ export interface SubJson {
 	kind: string
 	data: Post
 }
+
+export interface Comment {
+	all_awardings: []
+	approved_at_utc: string
+	approved_by: string
+	archived: boolean
+	associated_award: string
+	author: string
+	/*
+	author_flair_background_color: string
+	author_flair_css_class: string
+	author_flair_richtext: []
+	author_flair_template_id: "7be44c6e-be39-11e6-b398-0eae18c336b8"
+	author_flair_text: ":flag-us: America"
+	author_flair_text_color: "dark"
+	author_flair_type: "richtext"
+	author_fullname: "t2_50zb18iw"
+	author_patreon_flair: boolean
+	author_premium: boolean
+	*/
+	awarders: []
+	banned_at_utc: string
+	banned_by: string
+	body: string
+	body_html: string
+	can_gild: boolean
+	can_mod_post: boolean
+	collapsed: boolean
+	collapsed_because_crowd_control: string
+	collapsed_reason: string
+	controversiality: number
+	created: number
+	created_utc: number
+	depth: number
+	distinguished: string
+	downs: number
+	edited: boolean
+	gilded: number
+	gildings: {}
+	id: string
+	is_submitter: boolean
+	likes: string
+	link_id: string
+	locked: boolean
+	mod_note: string
+	mod_reason_by: string
+	mod_reason_title: string
+	mod_reports: []
+	name: string
+	no_follow: boolean
+	num_reports: string
+	parent_id: string
+	permalink: string
+	removal_reason: string
+	//replies: {kind: "Listing", data: {…}}
+	report_reasons: string
+	saved: boolean
+	score: number
+	score_hidden: boolean
+	send_replies: boolean
+	stickied: boolean
+	subreddit: string
+	subreddit_id: string
+	subreddit_name_prefixed: string
+	subreddit_type: string
+	total_awards_received: number
+	treatment_tags: []
+	ups: number
+	user_reports: []
+}
