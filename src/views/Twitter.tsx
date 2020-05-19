b3fa127be5cfa3d21e75a05368447a227d772806
Brent Ely <gitbrent@gmail.com>
2020-05-19 00:01:17 -0500

    added twitter and firebase pkgs

--- src/views/Twitter.tsx
+++ src/views/Twitter.tsx
@@ -1,15 +1,62 @@
 import React, { useState, useEffect } from 'react'
-/*
-const Twitter = require('twitter');
-const client = new Twitter({
-  consumer_key: 'xxxxxxx',
-  consumer_secret: 'xxxxxxxx',
-  access_token_key: 'xxxxxxx',
-  access_token_secret: 'xxxxxxxx',
-})
-*/
-export default function Twitter() {
-	return <h5>(need developer key)</h5>
+import Twitter from 'twitter-lite'
+import * as firebase from 'firebase'
+import 'firebase/database'
+
+export default function TabTwitter() {
+	const firebaseConfig = {
+		apiKey: 'AIzaSyB1Ey2f2xyBAaU5lbJwWBWX9LoZjakF-Ag',
+		authDomain: 'wutsupdash.firebaseapp.com',
+		databaseURL: 'https://wutsupdash.firebaseio.com',
+		projectId: 'wutsupdash',
+		storageBucket: 'wutsupdash.appspot.com',
+		messagingSenderId: '280692246272',
+		appId: '1:280692246272:web:6308584170b72fe11e55e5',
+		measurementId: 'G-MBHW8T74RD',
+	}
+
+	const [firebaseApp, setFirebaseApp] = useState<firebase.app.App>()
+
+	useEffect(() => {
+		if (!firebaseApp) setFirebaseApp(firebase.initializeApp(firebaseConfig))
+	}, [firebaseApp, firebaseConfig])
+
+	function getTrending() {
+		/*
+			@link https://developer.twitter.com/en/docs/trends/trends-for-location/api-reference/get-trends-place
+
+			// Trending:
+			// global: 1
+			// dallas: 2388929 (https://www.findmecity.com/)
+			GET https://api.twitter.com/1.1/trends/place.json?id=1
+		*/
+	}
+
+	async function test() {
+		console.log(firebaseApp)
+
+		const user = new Twitter({
+			consumer_key: 'USE_FIREBASE',
+			consumer_secret: 'USE_FIREBASE',
+		})
+
+		const response = await user.getBearerToken()
+		console.log(response)
+		/*
+		const app = new Twitter({
+			bearer_token: response.access_token,
+		})
+		*/
+	}
+
+	return (
+		<section>
+			<h5>(need developer key)</h5>
+			<button type='button' onClick={() => test()}>
+				Run Test
+			</button>
+		</section>
+	)
 }
 /*
 class TwitterFeed extends Component {
